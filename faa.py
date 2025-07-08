1b0a80fd6346ee385fb34e82b94a29fb5ed7acb9
gooowl <gooowl@owls.com>
2025-07-08 14:25:43 -0700

    fixed FAA base API change

--- faa.py
+++ faa.py
@@ -6,31 +6,41 @@ http = urllib3.PoolManager()
 
 class AirportStatus:
     def __init__(self, airport_code):
-        self.API_URL = "https://soa.smext.faa.gov/asws/api/airport/status/{}"
+        self.API_URL = "https://nasstatus.faa.gov/api/airport-events"
         self.details = []
         self.airport_delays(airport_code)
         self._display()
+    def _get_data_by_airport(self, response, airport_code):
+        airport_data = {}
+        for airport in response:
+            if airport.get('airportId').upper() == airport_code.upper():
+                airport_data['Name'] = airport.get('airportLongName')
+                airport_data['IATA'] = airport.get('airportId')
+                airport_data['Delay'] = airport.get('groundDelay', dict()).get('avgDelay', '')
+                airport_data['Weather'] = airport.get('groundDelay', dict()).get('impactingCondition', '')
+                airport_data['Status'] = airport.get('departureDelay', 'NA')
+        return airport_data
 
     def _get_faa_data(self, airport_code):
         r = http.request('GET', self.API_URL.format(airport_code))
-        data = {}
+        response = {}
         if r.status == 200:
-            data = r.data
-        return data
+            response = json.loads(r.data)
+            response = self._get_data_by_airport(response, airport_code)
+        return response
 
     def airport_delays(self, airport_code):
         self.details.append(self._get_faa_data(airport_code))
 
     def _display(self):
         for airport in self.details:
-            resp = json.loads((airport.decode('utf-8')))            
-            print ("{}, {}, {} ::: Delays : {} \nStatus : {}\nWeather : {}".format(
+            resp = airport
+            print ("{}, {} ::: Delay (mins) : {} \nStatus : {}\nWeather : {}".format(
                 resp.get('Name'),
             	resp.get('IATA'),
-            	resp.get('City'),
             	resp.get('Delay'),
-            	resp.get('Status')[0].get('Reason'),
-                resp.get('Weather').get('Weather'),
+            	resp.get('Status', 'NA'),
+                resp.get('Weather'),
                 )
             )
 
